33cded23c35756daa878b792321211d930c5f728
xfile6912 <xfile6912@naver.com>
2019-12-22 12:24:41 +0900

    modify do function to make snake do not move when opposite site key pressed.

--- snakegame.c
+++ snakegame.c
@@ -1,5 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -8,7 +7,9 @@
 
 int time_out, cx, cy, game_over;
 int lastc;
+int speed = 200;
 int arr[10000][10000];
+int snake[10000][10000];
 int  foodx, foody;//먹이의 위치
 int score = 0;//점수이면서 동시에 뱀의 길이
 int getfood;//0이면 아직먹이를 못먹은상태,1이면 먹은상태
@@ -43,8 +44,17 @@ void makeFood(int n, int m)//먹이를 출력시킬 임의의 위치를 생성해 foodx, foody에
 }
 void init(int n, int m)//먹이와 뱀의 초기위치 설정, 뱀의 초기위치는 맵의 정중앙 cx,cy;
 {
+	int i, j;
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			snake[i][j] = 0;
+		}
+	}
 	cx = n / 2;
 	cy = m / 2;
+	snake[cy][cx] = 1;
 	game_over = 0;
 	time_out = 0;
 	getfood = 0;
@@ -52,31 +62,35 @@ void init(int n, int m)//먹이와 뱀의 초기위치 설정, 뱀의 초기위치는 맵의 정중앙 
 }
 void Do(int sig, int n, int m)//main함수에 의해 0.3초마다 호출된다. 현재 뱀의 진행방향에 따라 다음화면에 출력될 뱀과 먹이의 위치 설정
 {
-	if (sig == 'w')
+	if (sig == 'w' && lastc != 's')//이전키와 반대방향의 키가 나오지않는 경우에만 이동함.
 	{
+		lastc = sig;
 		cy--;
 	}
-	else if (sig == 'd')
+	else if (sig == 'd' && lastc != 'a')
 	{
+		lastc = sig;
 		cx++;
 	}
-	else if (sig == 'a')
+	else if (sig == 'a' && lastc != 'd')
 	{
+		lastc = sig;
 		cx--;
 	}
-	else if (sig == 's')
+	else if (sig == 's' && lastc != 'w')
 	{
+		lastc = sig;
 		cy++;
 	}
 	if (cx == foodx && cy == foody)//먹이를 먹음;
 		getfood = 1;
-	if (cx == 0 || cy == 0 || cx == n || cy == m)
+	if (cx == 0 || cy == 0 || cx == n - 1 || cy == m - 1)
 		game_over = 1;
 }
 int getCommand(void)//사용자로부터 입력받은키를 반환해준다.
 {
 	int c = 0;
-	if (_kbhit())
+	if (_kbhit())//입력을 받지않으면 이동하던 방향으로 계속 이동
 		c = _getch();
 	else
 		c = lastc;
@@ -86,8 +100,8 @@ int main(void)
 {
 	FILE* ipt = fopen("board.txt", "r");
 	int res;
-	int n, m, j, i;
 	int command;
+	int n, m, j, i;
 	int temp;//엔터를 받아줌
 	res = fscanf(ipt, "%d %d", &n, &m);
 	for (i = 0; i < m; i++)
@@ -102,14 +116,15 @@ int main(void)
 	init(n, m);//뱀의 초기위치 초기화
 	srand(time(NULL));
 	do {
-		Sleep(300);
+		Sleep(speed);
 		command = getCommand();
-		lastc = command;
+		printf("%d", game_over);
 		Do(command, n, m);
 		if (getfood == 1)
 		{
 			makeFood(n, m);
 			score++;
+			speed -= 7;//먹이를 먹으면 스피드가 빨라짐.
 			getfood = 0;
 		}
 		drawBoard(n, m);
